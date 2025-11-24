d0d44b93bb105377574c1f6e51ffb53e8f6bba23
adammKass <adam.kascak@protonmail.com>
2025-11-25 00:02:58 +0100

    -lenis

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,50 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import Navbar from "./components/Navbar";
-import Section from "./components/Section";
 import { heroSlides } from "./constants";
 import { ScrollTrigger, SplitText, ScrollToPlugin } from "gsap/all";
-import { useEffect, useLayoutEffect } from "react";
+import { ReactLenis } from "lenis/react";
+import { useEffect, useLayoutEffect, useRef } from "react";
 
 gsap.registerPlugin(ScrollTrigger, SplitText, ScrollToPlugin);
 
 const App = () => {
-  useLayoutEffect(() => {
+  useGSAP(() => {
     const sections = gsap.utils.toArray("section");
+    let snapPoints = [0, 0.33, 0.66, 1];
 
-    sections.forEach((section) => {
-      ScrollTrigger.create({
-        trigger: section,
-        start: "top bottom-=15%",
-        end: "bottom top+=15%",
-        onEnter: () => {
-          gsap.to(window, {
-            scrollTo: section.offsetTop,
-            duration: 1.4,
-            ease: "power3.out",
-          });
-        },
-        onEnterBack: () => {
-          gsap.to(window, {
-            scrollTo: section.offsetTop,
-            duration: 1.3,
-            ease: "power3.out",
-          });
-        },
-      });
+    ScrollTrigger.create({
+      trigger: "main",
+      start: "top top",
+      end: "bottom bottom",
+      snap: {
+        snapTo: snapPoints,
+        duration: 1,
+        ease: "power3.inOut",
+        delay: 0,
+      },
     });
+  });
 
-    ScrollTrigger.refresh();
+  const lenisRef = useRef();
+
+  useEffect(() => {
+    function update(time) {
+      lenisRef.current?.lenis?.raf(time * 800);
+    }
+
+    gsap.ticker.add(update);
+
+    return () => gsap.ticker.remove(update);
   }, []);
 
   return (
     <>
+      <ReactLenis root options={{ autoRaf: false }} ref={lenisRef} />
       <Navbar></Navbar>
-      <main className="min-h-dvh h-dvh">
+      <main>
         {heroSlides.map((slide, index) => (
-          <section key={index} className="relative h-lvh bg-amber-600">
+          <section key={index} className="relative h-screen bg-amber-600">
             <div className="absolute inset-0">
               <picture>
                 <source srcSet={slide.imageMobile} media="(max-width: 768px)" />
